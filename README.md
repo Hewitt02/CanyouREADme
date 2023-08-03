41dbf328b944d6d6b7b50c32a0f07139b3c4e1cc
Hewitt02 <Hewitt1203@gmail.com>
2023-08-03 19:13:30 -0400

    adding questions array

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CanyouREADme
+
+This app is a README file generator. The motivation for making this app is to generate README files for people that need a professional README. I built this to further my knowledge with using Javascript and node. Making this project enhanced my skills in Javascript. The problem I solved is to be able to genrate README files. 
--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,10 +1,16 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questionsArray = [
+ questionsArray.push("What was your motivation");
+ questionsArray.push("Why did you build this project?");
+ questionsArray.push("What problem does it solve?");
+ questionsArray.push("What did you learn?");
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeREADMEFile(fileName, data) {
+
+};
 
 // TODO: Create a function to initialize app
 function init() {}
